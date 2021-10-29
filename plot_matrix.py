21798aaff36657a156bb6be77674d416d0bb50c1
piglet94 <715654107@qq.com>
2021-10-29 16:23:07 +0800

    作业3

--- plot_matrix.py
+++ plot_matrix.py
@@ -1,14 +1,19 @@
 # TODO: import ...
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_random_matrix(m,n):
+    matrix=np.random.randint(0,2,(m,n))
+    return matrix
 
 
-def generate_random_matrix(m, n):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
-
-
-def save_matrix(matrix, file_name):
-    raise NotImplementedError  # TODO: 删除该行，实现该函数功能
-
+def save_matrix(matrix,picture_name):
+    plt.imshow(matrix)
+    plt.savefig(picture_name)
+    
 
 if __name__ == "__main__":
-    matrix = generate_random_matrix(10, 10)
-    save_matrix(matrix, "example.jpg")
+    matrix = generate_random_matrix(10,10)
+    save_matrix(matrix,"example.jpg")
+
+# pytest()
