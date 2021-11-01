317335daef627a511d9fa823c037f917c0c89da2
piglet94 <715654107@qq.com>
2021-11-01 12:17:32 +0800

    作业3-4

--- plot_matrix.py
+++ plot_matrix.py
@@ -2,12 +2,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_random_matrix(m,n):
+
+def generate_random_matrix(m, n):
     matrix=np.random.randint(0,2,(m,n))
     return matrix
+
+
 def save_matrix(matrix,picture_name):
     plt.imshow(matrix)
-    plt.savefig(picture_name)    
+    plt.savefig(picture_name)
+
+
 if __name__=="__main__":
     matrix=generate_random_matrix(10,10)
     save_matrix(matrix,"example.jpg")
