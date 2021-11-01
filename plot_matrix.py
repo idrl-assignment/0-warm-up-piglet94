e2e0f75732e9d5512633cb6e12338fd837f61183
piglet94 <715654107@qq.com>
2021-11-01 11:56:32 +0800

    作业3-2

--- plot_matrix.py
+++ plot_matrix.py
@@ -6,14 +6,10 @@ def generate_random_matrix(m,n):
     matrix=np.random.randint(0,2,(m,n))
     return matrix
 
-
 def save_matrix(matrix,picture_name):
     plt.imshow(matrix)
     plt.savefig(picture_name)
     
-
 if __name__ == "__main__":
-    matrix = generate_random_matrix(10,10)
+    matrix=generate_random_matrix(10,10)
     save_matrix(matrix,"example.jpg")
-
-# pytest()
